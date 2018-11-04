22fafaaba987f6db5178f33d87cd2b3810caba5b
recluse27 <lazukav@gmail.com>
2018-11-04 01:59:04 -0800

    Initial commit

---
+++ test_clarifai.py
@@ -0,0 +1,24 @@
+__author__ = 'Toshiba'
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+
+app = ClarifaiApp(api_key='26e76b22e8394454a9e38befc4ac6fb5')
+model = app.models.get('face-v1.3')
+image = ClImage(url='https://samples.clarifai.com/face-det.jpg')
+m = model.predict([image])
+
+# print(m['status']['description'] == "Ok") #CHECK
+# print(m['outputs'][0]['data']['regions'])
+
+
+def get_boxes(m):
+    boxes = []
+
+    for i in m['outputs'][0]['data']['regions']:
+        boxes.append(i['region_info']['bounding_box'])
+        # print(i)
+    return boxes
+
+
+print(get_boxes(m))
